2a2741d1f430dd8ad1b4d40a78ec8e52785723c7
IagoDantas <iagodantas.8@gmail.com>
2024-01-30 09:38:19 -0300

    revert "chore: .env setup"
    This reverts commit 549ecd6d5cf8f919b8567797c47d2a9d5621f969.

--- src/env.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-const envSchema = z.object({
-  PORT: z.string(),
-  SPOTIFY_CLIENT_ID: z.string(),
-  SPOTIFY_CLIENT_SECRET: z.string(),
-});
-
-export const env = envSchema.parse(process.env);
