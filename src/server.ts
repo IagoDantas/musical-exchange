450ece548099572fb9bcb93ee8de6b893cf390be
IagoDantas <iagodantas.8@gmail.com>
2024-01-30 08:03:53 -0300

    🎉 chore: initial setup

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from "fastify";
+import fastifyCors from "@fastify/cors";
+
+const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+app
+  .listen({
+    port: 3333,
+    host: "0.0.0.0",
+  })
+  .then(() => {
+    console.log("Server is running on port 3333");
+  });
