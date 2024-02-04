271dc0be7dd90b7dd24fe9607a417c15b13aacfc
IagoDantas <iagodantas.8@gmail.com>
2024-02-03 22:11:59 -0300

    feat: add playlists by user

---
+++ fe/components/ui/playlist.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import { api } from "@/lib/axios";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+interface PlaylistProps {
+  accessToken: string;
+}
+
+type Images = {
+  url: string;
+
+}
+
+type Playlist = {
+  id: string;
+  name: string;
+  images: Images[];
+  href: string;
+  uri: string;
+}
+
+
+export function Playlist({ accessToken }: PlaylistProps) {
+
+  const [playlist, setPlaylist] = useState<Playlist[]>([]);
+
+  useEffect(() => {
+    const fetchPlaylists = async () => {
+      try {
+        const response = await api.get('https://api.spotify.com/v1/me/playlists', {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+        const playlists = response.data.items;
+        setPlaylist(playlists);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchPlaylists();
+  }, [accessToken]);
+
+  console.log(playlist);
+
+
+  return (
+    <div className="h-sreen w-screen flex-1 flex flex-row flex-wrap">
+      {
+        playlist.map((playlist) => (
+          <div key={playlist.id} className="w-1/2 flex">
+            <Image
+              src={playlist.images[0].url}
+              width="300"
+              height="300"
+              alt={playlist.name}
+            />
+            <h3>{playlist.name}</h3>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
